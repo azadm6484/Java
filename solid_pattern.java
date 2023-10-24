4d368d7effa5f88b7aae7445431ba1c4a90ad3c4
azadm6484 <148337651+azadm6484@users.noreply.github.com>
2023-10-24 13:26:43 +0530

    Add files via upload

---
+++ solid_pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class solid_pattern {
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.print("enter a number:");
+        int num=in.nextInt();
+        for(int i=1;i<=num+1;i++)
+        {
+            for(int j=1;j<=num+1;j++)
+            {
+                System.out.print("*");
+            }
+
+        System.out.println();
+        }
+        }
+    }
