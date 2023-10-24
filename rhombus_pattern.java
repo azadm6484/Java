4d2639c7f4d13d15a0874f2314d172c6ff52b1e8
azadm6484 <148337651+azadm6484@users.noreply.github.com>
2023-10-24 13:01:51 +0530

    Add files via upload

---
+++ rhombus_pattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class rhombus_pattern {
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.print("enter a number:");
+        int num=in.nextInt();
+        for(int i=1;i<=num+1;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=num+1;j++)
+            {
+                System.out.print("*");
+            }
+
+        System.out.println();
+        }
+        }
+    }
